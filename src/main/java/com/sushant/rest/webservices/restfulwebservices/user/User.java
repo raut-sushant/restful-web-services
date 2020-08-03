ad4109db9639f81d2c61917f6c00b4d9cba18b7f
Sushant Raut <raut.sushant@gmail.com>
2020-08-03 11:51:20 -0500

    Swagger refactor

--- src/main/java/com/sushant/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/sushant/rest/webservices/restfulwebservices/user/User.java
@@ -1,16 +1,22 @@
 package com.sushant.rest.webservices.restfulwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel (description = "Information about User.")
 public class User {
 
     private Integer id;
-    @Size(min = 2, message = "Name should be atleast 2 character.")
+    @Size(min = 2, message = "Name should be atleast 2 characters.")
+    @ApiModelProperty(notes = "Name should have atleast 2 characters.")
     private String name;
 
     @Past(message = "Please Enter past date as birthdate.")
+    @ApiModelProperty(notes = "Birth date should be in past.")
     private Date birthDate;
 
     protected User ( ) {
