56c5ca265c442b49ee97e3036675966e69726b13
Sushant Raut <raut.sushant@gmail.com>
2020-08-02 23:51:48 -0500

    Swagger refactor

--- src/main/java/com/sushant/rest/webservices/restfulwebservices/SwaggerConfig.java
+++ src/main/java/com/sushant/rest/webservices/restfulwebservices/SwaggerConfig.java
@@ -7,7 +7,6 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 import java.util.Arrays;
 import java.util.HashSet;
